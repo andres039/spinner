fee477acb8bc161cd92867dabb8cbf549d840ef0
Andres <andres039039@hotmail.com>
2021-11-02 14:43:01 -0400

    code optimized

---
+++ spinner2.js
@@ -0,0 +1,9 @@
+const spinnerState = ['\r|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  ', '\n']
+let timer = 0
+for(let spin of spinnerState) {
+  timer += 200;
+setTimeout(() => {
+  process.stdout.write(spin)
+}, timer);
+}
+
